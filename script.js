cb0353e46c5f64f164e38574904c71eb20d67fde
sivaprasadvitta <sivaprasad.vitta@gmial.com>
2024-03-26 18:37:34 +0530

    just complete HomePage

---
+++ script.js
@@ -0,0 +1,42 @@
+const imgs =document.querySelectorAll('.header-slider ul img')
+const prev_btn = document.querySelector('.control_prev');
+const next_btn = document.querySelector('.control_next');
+
+let n=0;
+for(let i=0;i<imgs.length;i++){
+    console.log(imgs[i]);
+}
+
+function changeSlide(){
+    for(let i=0;i<imgs.length;i++){
+        imgs[i].style.display = 'none';
+    }
+    imgs[n].style.display = 'block';
+}
+prev_btn.addEventListener("click",(e)=>{
+    if(n>0)
+        n--;
+    else{
+        n=imgs.length-1;
+    }
+    changeSlide();
+})
+next_btn.addEventListener("click",(e)=>{
+    console.log(n);
+    if(n<imgs.length-1){
+        n++;
+    }else{
+        n=0;
+    }
+    changeSlide();
+})
+
+
+const scrollContainer = document.querySelectorAll("products");
+
+for(const item of scrollContainer){
+    item.addEventListener("wheel",(e)=>{
+        e.preventDefault(); //preventing default event
+        item.scrollLeft+=e.deltaY; 
+    })
+}
